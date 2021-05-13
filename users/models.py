228a7949634518ffaa58ccf26530f50c4634b596
Алексей Ю, ККСО-17-3 <v_koralina@list.ru>
2021-05-14 02:34:17 +0500

    add verbose_name

--- users/models.py
+++ users/models.py
@@ -1,17 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
-from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
 
 from .managers import CustomUserManager
 
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
-    first_name = models.CharField(max_length=128, db_index=True)
-    last_name = models.CharField(max_length=128, db_index=True)
-    phone = models.CharField(max_length=12, unique=True)
-    email = models.EmailField(_('email address'), unique=True)
+    class Meta:
+        verbose_name = 'Пользователи'
+        verbose_name_plural = 'Пользователи'
+    first_name = models.CharField(max_length=128, db_index=True, verbose_name='Имя')
+    last_name = models.CharField(max_length=128, db_index=True, verbose_name='Фамилия')
+    phone = models.CharField(max_length=12, unique=True, verbose_name='Телефон')
+    email = models.EmailField(unique=True, verbose_name='Почта')
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     date_joined = models.DateTimeField(default=timezone.now)
