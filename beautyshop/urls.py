baa2a13863c108b8cc75e920598532b92064a47f
Алексей Ю, ККСО-17-3 <v_koralina@list.ru>
2021-05-17 00:14:42 +0500

    added test module for quick tests

--- beautyshop/urls.py
+++ beautyshop/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     path('shop/', include('shop.urls')),
     path('users/', include('users.urls')),
     path('cart/', include('cart.urls')),
+    path('test/', include('testmodule.urls')),
     path('order/', include('order.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
